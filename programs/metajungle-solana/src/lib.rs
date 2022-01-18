177953268c5a6712e5f4ae01cca98dff7344e1a0
JustaJunk <tcfsh.crazer@gmail.com>
2022-01-19 01:03:43 +0800

    add switchboard

--- programs/metajungle-solana/src/lib.rs
+++ programs/metajungle-solana/src/lib.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
 use spl_token::instruction::AuthorityType;
+use switchboard_aggregator::AggregatorAccountData;
+use std::convert::TryInto;
 
 declare_id!("AbpUUoHVrTpf1S9dbr5BAeMT5dL7YieReEwmqBNsxL9i");
 
@@ -11,9 +13,9 @@ pub mod anchor_escrow {
     pub fn create(ctx: Context<Create>) -> ProgramResult {
         ctx.accounts.trading_account.creater = ctx.accounts.creater.key();
         ctx.accounts.trading_account.is_open = false;
-        ctx.accounts.trading_account.open_price = 0;
+        ctx.accounts.trading_account.open_price = 0.0;
         ctx.accounts.trading_account.leaverage = 0;
-        ctx.accounts.trading_account.wealth = 1000;
+        ctx.accounts.trading_account.wealth = 1.0;
 
         // TODO: mint NFT at the same time
         
@@ -22,25 +24,26 @@ pub mod anchor_escrow {
 
     pub fn open_position(ctx: Context<OpenPosition>, leverage: i8) -> ProgramResult {
         ctx.accounts.trading_account.is_open = true;
-        ctx.accounts.trading_account.oracle_account = ctx.accounts.oracle_account.key();
-        // TODO: use swichboard oracle to get price
-        // ctx.accounts.trading_account.open_price = ;
+        let oracle_account = &ctx.accounts.oracle_account;
+        ctx.accounts.trading_account.oracle_account = oracle_account.key();
+        let feed_result = AggregatorAccountData::new(oracle_account)?.get_result()?;
+        ctx.accounts.trading_account.open_price = feed_result.try_into()?;
         ctx.accounts.trading_account.leaverage = leverage;
         Ok(())
     }
 
     pub fn close_position(ctx: Context<ClosePosition>) -> ProgramResult {
         ctx.accounts.trading_account.is_open = false;
-        let open_price = ctx.accounts.trading_account.open_price as i64;
-        let oracle_account = ctx.accounts.trading_account.oracle_account;
-        // TODO: use swichboard oracle to get price
-        // let close_price = ;
-        let close_price = 0 as i64;
-        let leverage = ctx.accounts.trading_account.leaverage as i64;
-        let current_wealth = ctx.accounts.trading_account.wealth as i64;
-        let gain = ((close_price - open_price) * leverage * current_wealth) / (open_price * 10);
-        ctx.accounts.trading_account.wealth = (current_wealth as i64 + gain) as u32;
+        let open_price: f64 = ctx.accounts.trading_account.open_price;
+        let oracle_account = &ctx.accounts.oracle_account;
+        let feed_result = AggregatorAccountData::new(oracle_account)?.get_result()?;
+        let close_price: f64 = feed_result.try_into()?;
+        let leverage: f64 = ctx.accounts.trading_account.leaverage.into();
+        let current_wealth: f64 = ctx.accounts.trading_account.wealth;
+        ctx.accounts.trading_account.wealth += ((close_price - open_price) * (leverage / 10.0) * current_wealth) / open_price;
+
         // TODO: update metadata of NFT
+
         Ok(())
     }
 }
@@ -73,10 +76,12 @@ pub struct OpenPosition<'info> {
 pub struct ClosePosition<'info> {
     #[account(signer)]
     pub creater: AccountInfo<'info>,
+    pub oracle_account: AccountInfo<'info>,
     #[account(
         mut,
         constraint = trading_account.creater == creater.key(),
         constraint = trading_account.is_open,
+        constraint = trading_account.oracle_account == oracle_account.key(),
     )]
     pub trading_account: Account<'info, TradingAccount>,
     #[account(mut)]
@@ -88,7 +93,7 @@ pub struct TradingAccount {
     pub creater: Pubkey,
     pub is_open: bool,
     pub oracle_account: Pubkey,
-    pub open_price: u32,
+    pub open_price: f64,
     pub leaverage: i8,
-    pub wealth: u32,
+    pub wealth: f64,
 }
